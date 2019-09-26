b7006f1b2a3d61104947ef7602344752cbf3ab3a
Evan <evanojackson@gmail.com>
2019-09-25 22:41:49 -0500

    working on replit lol

--- POSIX Shell/main.cpp
+++ POSIX Shell/main.cpp
@@ -1,19 +1,74 @@
-#include <iostream>
-#include <fstream>
-#include <sys/types.h>
+//
+//  main.cpp
+//  POSIXSHELL
+//
+//  Created by Evan Jackson on 9/24/19.
+//  Copyright © 2019 Evan Jackson. All rights reserved.
+//
+
+#include <stdio.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    char process;
-    int position;
-    cin >> process >> position;
-    while (process != '%'){
-        if (fork() == 0)
-            cout << "Hello from child!\n";
-        else
-            cout << "Hello from parent!\n";
+    cout << "hello\n";
+    //open this CPP file
+    char input;
+    int index, fd;
+    char byte;
+    scanf("%c",&input);
+    scanf("%d",&index);
+    fd = open("main.cpp", O_RDONLY);
+    
+    while(input != '%' && input == 'P') {
+        int child1 = fork();
+        int child2 = fork();
+
+        if(child1>0 && child2>0) { // is original parent
+            
+            int child4 = fork();
+            if(child4==0) {
+                //fourth child
+                lseek(fd,index + 3,SEEK_SET);
+                read(fd, &byte, 1);
+                printf("%c\n", byte);
+                return 0;
+            }
+            //get more input. look to end of else/if group.
+            
+        }
+        else if(child1 == 0&& child2 > 0) {
+            //first child
+            lseek(fd,index,SEEK_SET);
+            read(fd, &byte, 1);
+            printf("%c\n", byte);
+            return 0;
+        }
+        else if(child1 > 0 && child2 == 0) {
+            //second child
+            lseek(fd,index + 1,SEEK_SET);
+            read(fd, &byte, 1);
+            printf("%c\n", byte);
+            return 0;
+        }
+        else {
+            //third child
+            lseek(fd,index + 2,SEEK_SET);
+            read(fd, &byte, 1);
+            printf("%c\n", byte);
+            return 0;
+            
+        }
+        //get input again, since this is the parent process
+        scanf(" %c",&input);
+        scanf(" %d",&index);
     }
+    close(fd);
+    return 0;
 }
